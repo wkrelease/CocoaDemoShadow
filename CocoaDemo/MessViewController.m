2a53f6168e5eae673eac9f9013eff838ccbf976a
王坤成 <18501987683@163.com>
2016-08-29 17:21:04 +0800

    111
    1111

--- CocoaDemo/MessViewController.m
+++ CocoaDemo/MessViewController.m
@@ -1,28 +1,29 @@
 //
-//  RootViewController.m
+//  MessViewController.m
 //  CocoaDemo
 //
 //  Created by Talkmate on 16/8/29.
 //  Copyright © 2016年 CocoaKC. All rights reserved.
 //
 
-#import "RootViewController.h"
 #import "MessViewController.h"
 
-@interface RootViewController ()
+@interface MessViewController ()
 
 @end
 
-@implementation RootViewController
+@implementation MessViewController
 
 #pragma mark - LifeCycle
 
 - (void)viewDidLoad {
  
     [super viewDidLoad];
-    
-    self.view.backgroundColor = [UIColor lightGrayColor];
 
+    
+    self.view.backgroundColor = [UIColor grayColor];
+    
+    
 }
 
 @end
