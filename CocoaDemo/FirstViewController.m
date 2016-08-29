235a25681bc64ecef3cf47341053a887f41744e6
王坤成 <kuncheng_wang@talkmate.com>
2016-08-30 01:10:52 +0800

    222
    222

--- CocoaDemo/FirstViewController.m
+++ CocoaDemo/FirstViewController.m
@@ -16,7 +16,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+
+
+    self.view.backgroundColor = [UIColor cyanColor];
+
 }
 
 - (void)didReceiveMemoryWarning {
