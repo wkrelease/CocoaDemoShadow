a0c4c496a0d1fe39d303d2635673088d6326f71f
王坤成 <kuncheng_wang@talkmate.com>
2016-08-29 12:08:13 +0800

    Initial Commit

---
+++ CocoaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoaDemo
+//
+//  Created by Talkmate on 16/8/29.
+//  Copyright © 2016年 CocoaKC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
