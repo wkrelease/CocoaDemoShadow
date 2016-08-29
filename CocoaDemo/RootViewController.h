648e7395815efd67a6cd7ecf0a2c699b6ed29b88
王坤成 <18501987683@163.com>
2016-08-29 15:39:00 +0800

    first
    add file

--- CocoaDemo/RootViewController.h
+++ CocoaDemo/RootViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  RootViewController.h
 //  CocoaDemo
 //
 //  Created by Talkmate on 16/8/29.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface RootViewController : UIViewController
 
 @end
-
---
+++ CocoaDemo/RootViewController.m
@@ -0,0 +1,27 @@
+//
+//  RootViewController.m
+//  CocoaDemo
+//
+//  Created by Talkmate on 16/8/29.
+//  Copyright © 2016年 CocoaKC. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+#pragma mark - LifeCycle
+
+- (void)viewDidLoad {
+ 
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor lightGrayColor];
+
+}
+
+@end
