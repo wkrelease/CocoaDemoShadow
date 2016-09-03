abc3f84045abffa2d6b5bd141271872e7ff2cb20
王坤成 <18501987683@163.com>
2016-09-03 16:27:01 +0800

    array
    array

--- CocoaDemo/MessViewController.m
+++ CocoaDemo/MessViewController.m
@@ -6,6 +6,14 @@
 //  Copyright © 2016年 CocoaKC. All rights reserved.
 //
 
+/**
+ *-----KVC
+ *
+ *
+ *
+ */
+
+
 #import "MessViewController.h"
 
 @interface MessViewController ()
@@ -19,9 +27,19 @@
 - (void)viewDidLoad {
  
     [super viewDidLoad];
-    
     self.view.backgroundColor = [UIColor grayColor];
+    [self setupMess];
     
+}
+
+#pragma mark － SetUp
+
+- (void)setupMess{
+    
+    //删除数组里的重复数据
+    NSArray *array = @[@"name", @"w", @"aa", @"zxp", @"aa"]; //返回的是一个新的数组
+    NSArray *newArray = [array valueForKeyPath:@"@distinctUnionOfObjects.self"];
+    NSLog(@"%@", newArray);
     
 }
 
